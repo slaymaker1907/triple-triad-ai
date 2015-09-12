7553aa2c22fc14b86460bfa8d6fff534cf238064
slaymaker1907 <dyllongagnier@gmail.com>
2015-09-12 14:31:50 -0600

    Added cardRarity and cardType

--- TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/card/Card.java
+++ TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/card/Card.java
@@ -9,13 +9,17 @@ public class Card
 	
 	public final int north, east, south, west;
 	public final Card.Type cardType;
+	public final String cardName;
+	public final int cardRarity;
 
-	public Card(int north, int east, int south, int west, Card.Type cardType)
+	public Card(int north, int east, int south, int west, String cardName, Card.Type cardType, int cardRarity)
 	{
 		this.north = north;
 		this.east = east;
 		this.south = south;
 		this.west = west;
 		this.cardType = cardType;
+		this.cardName = cardName;
+		this.cardRarity = cardRarity;
 	}
 }
