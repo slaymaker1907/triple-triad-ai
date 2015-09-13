e1f03933ac4d38fd70ad10afa76ea488743f728a
slaymaker1907 <dyllongagnier@gmail.com>
2015-09-13 09:47:13 -0600

    Added a controlling player field

--- TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/card/Card.java
+++ TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/card/Card.java
@@ -17,9 +17,10 @@ public class Card
 	public final Card.Type cardType;
 	public final String name;
 	public final int cardRarity;
+	public final Player holdingPlayer;
 
 	/**
-	 * This creates a new card object with the given parameters.
+	 * This creates a new card object with the given parameters. It will initialize the player to Player.None.
 	 * @param north The top number on a card.
 	 * @param east The right number on a card.
 	 * @param south The bottom number on a card.
@@ -44,6 +45,39 @@ public class Card
 		this.cardType = cardType;
 		this.name = cardName;
 		this.cardRarity = cardRarity;
+		this.holdingPlayer = Player.NONE;
+	}
+	
+	/**
+	 * This is a constructor which allows for every data type to be specified.
+	 * @param north The top number on a card.
+	 * @param east The right number on a card.
+	 * @param south The bottom number on a card.
+	 * @param west The left number on a card.
+	 * @param cardName The name of the card. Must be non-null and should be unique.
+	 * @param cardType The type/tribe of the card. Must be non-null.
+	 * @param cardRarity The rarity rarity of the card. Must be 1-5.
+	 * @param holdingPlayer The value to set holdingPlayer to. Must be non-null.
+	 */
+	public Card(int north, int east, int south, int west, String cardName, Card.Type cardType, int cardRarity, Player holdingPlayer)
+	{
+		if (cardName == null)
+			throw new NullPointerException();
+		if (cardType == null)
+			throw new NullPointerException();
+		if (holdingPlayer == null)
+			throw new NullPointerException();
+		if (cardRarity < 1 || cardRarity > 5)
+			throw new IllegalArgumentException("Card Rarity:" +  cardRarity);
+
+		this.north = north;
+		this.east = east;
+		this.south = south;
+		this.west = west;
+		this.cardType = cardType;
+		this.name = cardName;
+		this.cardRarity = cardRarity;
+		this.holdingPlayer = holdingPlayer;
 	}
 	
 	@Override
@@ -82,5 +116,16 @@ public class Card
 	public int hashCode()
 	{
 		return this.toString().hashCode();
+	}
+	
+	/**
+	 * This method returns a new Card without modifying the current card that is exactly the same as the
+	 * current card except for the player.
+	 * @param input The input player field.
+	 * @return A new card.
+	 */
+	public Card setHoldingPlayer(Player input)
+	{
+		return new Card(this.north, this.east, this.south, this.west, this.name, this.cardType, this.cardRarity, input);
 	}
 }
---
+++ TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/card/Player.java
@@ -0,0 +1,6 @@
+package com.dyllongagnier.triad.card;
+
+public enum Player
+{
+	SELF, OPPONENT, NONE;
+}
