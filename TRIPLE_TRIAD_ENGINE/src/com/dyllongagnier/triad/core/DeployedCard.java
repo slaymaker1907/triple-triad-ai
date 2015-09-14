c931a7f5fc282a9e685fe7b9c9d2341e4236fdac
Dyllon Gagnier <dyllongagnier@gmail.com>
2015-09-14 16:27:44 -0600

    Added some different ways to compare cards

--- TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/core/DeployedCard.java
+++ TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/core/DeployedCard.java
@@ -3,11 +3,29 @@ package com.dyllongagnier.triad.core;
 import com.dyllongagnier.triad.card.Card;
 import com.dyllongagnier.triad.card.UndeployedCard;
 
+/**
+ * This class represents a card that has been played on the board.
+ */
 public class DeployedCard 
 {
 	public final Card card;
 	public final int row, col;
 	
+	/**
+	 * This enum represents on of the four cardinal directions.
+	 */
+	public static enum Direction
+	{
+		NORTH, SOUTH, EAST, WEST
+	}
+	
+	/**
+	 * This method creates a new card with the specified parameters. It will deploy
+	 * the UndeployedCard.
+	 * @param card The card to play.
+	 * @param row The row of the card.
+	 * @param col The column of the card.
+	 */
 	public DeployedCard(UndeployedCard card, int row, int col)
 	{
 		assert BoardState.isInBounds(row, col);
@@ -15,5 +33,25 @@ public class DeployedCard
 		this.card = card.deploy();
 		this.row = row;
 		this.col = col;
+	}
+	
+	/**
+	 * This method returns the direction of the other card in relation to this card.
+	 * @param other The other card to compare. It should be one square away either left/right or
+	 * up/down.
+	 * @return The direction of the other card in relation to this card.
+	 */
+	public Direction getDirectionOfOther(DeployedCard other)
+	{
+		assert (Math.abs(other.row - this.row) == 1 && other.col == this.col) ||
+			(Math.abs(other.col - this.col) == 1 && other.row == this.row);
+		if (other.row > this.row)
+			return Direction.SOUTH;
+		else if (other.row < this.row)
+			return Direction.NORTH;
+		else if (other.col > this.col)
+			return Direction.EAST;
+		else
+			return Direction.WEST;
 	}
 }
--- TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/core/functions/DeployedCardComparators.java
+++ TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/core/functions/DeployedCardComparators.java
@@ -1,9 +1,60 @@
 package com.dyllongagnier.triad.core.functions;
 
+import java.util.Comparator;
+
+import com.dyllongagnier.triad.core.DeployedCard;
+
+/**
+ * This class contains various comparators for deployed cards for different rule sets.
+ */
 public class DeployedCardComparators 
 {
-	public boolean reverseCompare(DeployedCard playedCard, DeployedCard otherCard)
+	/**
+	 * This function compares playedCard with otherCard normally except that lower numbers beat
+	 * bigger numbers.
+	 * @param playedCard The card just played.
+	 * @param otherCard The card already on the field.
+	 * @return Whether the played card captures the other card.
+	 */
+	public static boolean reverseCompare(DeployedCard playedCard, DeployedCard otherCard)
 	{
-		return false;
+		Comparator<Integer> comp = (i1, i2) -> -1 * Integer.compare(i1, i2);
+		return DeployedCardComparators.compareCards(playedCard, otherCard, comp);
+	}
+	
+	/**
+	 * This function compares playedCard with otherCard normally with higher numbers beating smaller ones.
+	 * @param playedCard The card just played.
+	 * @param otherCard The card already on the field.
+	 * @return Whether the played card captures the other card.
+	 */
+	public static boolean regularCompare(DeployedCard playedCard, DeployedCard otherCard)
+	{
+		return DeployedCardComparators.compareCards(playedCard, otherCard, Integer::compare);
+	}
+	
+	/**
+	 * This function compares playedCard with otherCard using the supplied comparator to compare relevant
+	 * integer attributes of the cards.
+	 * @param playedCard The card just played.
+	 * @param otherCard The card already on the field.
+	 * @return Whether the played card captures the other card.
+	 */
+	private static boolean compareCards(DeployedCard playedCard, DeployedCard otherCard, Comparator<Integer> comp)
+	{
+		DeployedCard.Direction dir = playedCard.getDirectionOfOther(otherCard);
+		switch(dir)
+		{
+		case NORTH:
+			return comp.compare(playedCard.card.north, otherCard.card.south) > 0;
+		case EAST:
+			return comp.compare(playedCard.card.east, otherCard.card.west) > 0;
+		case SOUTH:
+			return comp.compare(playedCard.card.south, otherCard.card.north) > 0;
+		case WEST:
+			return comp.compare(playedCard.card.west, otherCard.card.east) > 0;
+		default:
+			throw new RuntimeException("Unsupported enum value.");
+		}
 	}
 }
