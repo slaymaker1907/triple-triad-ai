310ff08a52da89a2d7a19c5f59a7c05a8a9d8ec5
Dyllon Gagnier <dyllongagnier@gmail.com>
2015-09-21 12:52:59 -0600

    Finished rules tests except for one

--- TRIPLE_TRIAD_TEST/src/com/dyllongagnier/triad/core/RulesTest.java
+++ TRIPLE_TRIAD_TEST/src/com/dyllongagnier/triad/core/RulesTest.java
@@ -28,4 +28,66 @@ public class RulesTest
 		FunctionAsserts.assertEquals(DeployedCardComparator::reverseCompare, rules1.cardComparator);
 		FunctionAsserts.assertEquals(CardPlayFunction::plusCombo, rules1.playFunc);
 	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testComboNoOther()
+	{
+		new Rules(true, true, true, true, false, false, true, Rules.AscensionRule.NONE);
+	}
+	
+	@Test
+	public void testSamePlus()
+	{
+		Rules test = new Rules(true, true, true, true, true, true, false, Rules.AscensionRule.NONE);
+		FunctionAsserts.assertEquals(CardPlayFunction::samePlus, test.playFunc);
+	}
+	
+	@Test
+	public void testSameOnly()
+	{
+		Rules test = new Rules(true, true, true, true, true, false, false, Rules.AscensionRule.NONE);
+		FunctionAsserts.assertEquals(CardPlayFunction::sameCapture, test.playFunc);
+	}
+	
+	@Test
+	public void testPlusOnly()
+	{
+		Rules test = new Rules(true, true, true, true, false, true, false, Rules.AscensionRule.NONE);
+		FunctionAsserts.assertEquals(CardPlayFunction::plusCapture, test.playFunc);
+	}
+	
+	@Test
+	public void testSamePlusCombo()
+	{
+		Rules test = new Rules(true, true, true, true, true, true, true, Rules.AscensionRule.NONE);
+		FunctionAsserts.assertEquals(CardPlayFunction::samePlusCombo, test.playFunc);
+	}
+	
+	@Test
+	public void testBasicCapture()
+	{
+		Rules test = new Rules(true, true, true, true, false, false, false, Rules.AscensionRule.NONE);
+		FunctionAsserts.assertEquals(CardPlayFunction::samePlusCombo, test.playFunc);
+	}
+	
+	@Test
+	public void testNoTransform()
+	{
+		Rules test = new Rules(true, true, true, true, false, false, false, Rules.AscensionRule.NONE);
+		FunctionAsserts.assertEquals(AscensionTransform::noAscension, test.ascensionFunc);
+	}
+	
+	@Test
+	public void testDescension()
+	{
+		Rules test = new Rules(true, true, true, true, false, false, false, Rules.AscensionRule.DESCENSION);
+		FunctionAsserts.assertEquals(AscensionTransform::descension, test.ascensionFunc);
+	}
+	
+	@Test
+	public void testNoOrder()
+	{
+		Rules test = new Rules(true, false, true, true, false, false, false, Rules.AscensionRule.DESCENSION);
+		throw new UnsupportedOperationException();
+	}
 }
