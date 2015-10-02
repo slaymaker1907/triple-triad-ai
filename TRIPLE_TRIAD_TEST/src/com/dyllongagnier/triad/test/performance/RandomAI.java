53382746774a9733d7e4028dddeaa9b0a04e1c93
slaymaker1907 <dyllongagnier@gmail.com>
2015-10-01 19:35:32 -0600

    Added a Random AI

---
+++ TRIPLE_TRIAD_TEST/src/com/dyllongagnier/triad/test/performance/RandomAI.java
@@ -0,0 +1,25 @@
+package com.dyllongagnier.triad.test.performance;
+
+import java.util.List;
+import java.util.Random;
+
+import com.dyllongagnier.triad.core.GameAgent;
+import com.dyllongagnier.triad.core.GameControls;
+import com.dyllongagnier.triad.core.GameControls.PossibleMove;
+
+/**
+ * This is a random AI for the purpose of testing.
+ */
+public class RandomAI implements GameAgent
+{
+	private Random gen = new Random();
+	
+	@Override
+	public void takeTurn(GameControls controls)
+	{
+		List<PossibleMove> moves = controls.getValidMoves();
+		PossibleMove move = moves.get(gen.nextInt());
+		controls.playCard(move.toPlay, move.row, move.col);
+	}
+
+}
