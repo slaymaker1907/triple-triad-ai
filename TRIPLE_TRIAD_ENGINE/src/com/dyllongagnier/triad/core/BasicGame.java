a4f6e33d574920281ad32e694c525505b90478ce
slaymaker1907 <dyllongagnier@gmail.com>
2015-10-01 19:12:04 -0600

    Updated the BasicGame class to us BoardState.Builder

--- TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/core/BasicGame.java
+++ TRIPLE_TRIAD_ENGINE/src/com/dyllongagnier/triad/core/BasicGame.java
@@ -1,10 +1,8 @@
 package com.dyllongagnier.triad.core;
 
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 import com.dyllongagnier.triad.card.Player;
-import com.dyllongagnier.triad.card.UndeployedCard;
 
 public class BasicGame
 {
@@ -13,33 +11,28 @@ public class BasicGame
 	 * 
 	 * @param firstPlayerGen
 	 *            A function to determine the first player to play.
-	 * @param selfCards
-	 *            The cards for Player.SELF. Must contain 5 cards.
-	 * @param opponentCards
-	 *            The cards for Player.OPPONENT. Must contain 5 cards.
+	 * @param gameBuilder
+	 * 			  The game builder to use for this game.
 	 * @param selfAgent
 	 *            The agent for self. The agent to use for playing Player.SELF.
 	 * @param opponentAgent
 	 *            The agent for OPPONENT. The agent to use for playing
 	 *            Player.OPPONENT.
-	 * @param gameRules
-	 *            The rules to use for playing this game.
 	 * @return The final BoardState of the game.
 	 */
 	public static BoardState runGame(Supplier<Player> firstPlayerGen,
-			UndeployedCard[] selfCards, UndeployedCard[] opponentCards,
-			GameAgent selfAgent, GameAgent opponentAgent, Rules gameRules)
+			BoardState.Builder gameBuilder,
+			GameAgent selfAgent, GameAgent opponentAgent)
 	{
 		Player firstPlayer = firstPlayerGen.get();
 		assert firstPlayer != Player.NONE;
 
-		BoardState currentState = BasicGame.getInitialState(selfCards,
-				opponentCards, gameRules);
+		BoardState currentState = gameBuilder.build();
 		Player currentPlayer = firstPlayer;
 		for (int turn = 1; turn <= 9; turn++)
 		{
 			GameControls currentControls = new GameControls(currentState,
-					currentPlayer, gameRules.moveValidator);
+					currentPlayer, gameBuilder.getMoveValidator());
 			switch (currentPlayer)
 			{
 				case SELF:
@@ -58,39 +51,15 @@ public class BasicGame
 
 		assert currentState.gameComplete();
 
-		if (gameRules.isSuddenDeath)
+		if (gameBuilder.isSuddenDeath)
 		{
 			Player winner = currentState.getWinner();
 			if (winner == Player.NONE)
 			{
-				Function<Player, UndeployedCard[]> cardFunc = currentState
-						.getCardsUnderPlayers();
-				return BasicGame.runGame(firstPlayerGen,
-						cardFunc.apply(Player.SELF),
-						cardFunc.apply(Player.SELF), selfAgent, opponentAgent,
-						gameRules);
+				return BasicGame.runGame(firstPlayerGen, gameBuilder, selfAgent, opponentAgent);
 			}
 		}
 
 		return currentState;
-	}
-
-	/**
-	 * Initializes a BoardState with the input cards
-	 * 
-	 * @param selfCards
-	 *            The cards of Player.SELF. Must be 5 cards.
-	 * @param opponentCards
-	 *            The cards of Player.OPPONENT. Must be 5 cards.
-	 * @return A board state with no cards played and the input cards in the
-	 *         appropriate hands.
-	 */
-	public static BoardState getInitialState(UndeployedCard[] selfCards,
-			UndeployedCard[] opponentCards, Rules gameRules)
-	{
-		assert selfCards.length == 5 && opponentCards.length == 5;
-		return new BoardState.Builder(gameRules)
-				.setHand(Player.SELF, selfCards)
-				.setHand(Player.OPPONENT, opponentCards).build();
 	}
 }
