cbcd1dba54d46581879cce24a9d4da60e0a41b74
Dyllon Gagnier <dyllongagnier@gmail.com>
2015-10-30 17:04:30 -0600

    Supposed to be in last change

--- TRIPLE_TRIAD_GUI/src/com/dyllongagnier/triad/gui/controller/PlayerGenerator.java
+++ TRIPLE_TRIAD_GUI/src/com/dyllongagnier/triad/gui/controller/PlayerGenerator.java
@@ -1,46 +1,15 @@
 package com.dyllongagnier.triad.gui.controller;
 
-import java.awt.EventQueue;
-import java.lang.reflect.InvocationTargetException;
-
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
-
 import com.dyllongagnier.triad.card.Player;
 import com.dyllongagnier.triad.core.PlayerSupplier;
-import com.dyllongagnier.triad.gui.view.MainWindow;
+import com.dyllongagnier.triad.gui.view.GameView;
 
 public class PlayerGenerator implements PlayerSupplier
 {
-	private volatile Player toReturn;
-	
-	/**
-	 * This method should query the user for the first player when sudden death occurs or at the start of the game.
-	 * @return
-	 */
-	private static Player askUserForFirstPlayer()
-	{
-		Object[] possiblePlayers = new Object[]{Player.SELF, Player.OPPONENT};
-		return (Player)JOptionPane.showInputDialog(MainWindow.getMainWindow(), "Please enter the first player.", "First Player", JOptionPane.PLAIN_MESSAGE, null, possiblePlayers, Player.SELF);
-	}
-
 	@Override
 	public synchronized Player get()
-	{
-		// This may cause thread deadlock, be wary of this method.
-		try
-		{
-			if (!EventQueue.isDispatchThread())
-				SwingUtilities.invokeAndWait(() -> this.toReturn = PlayerGenerator.askUserForFirstPlayer());
-			else
-				this.toReturn = PlayerGenerator.askUserForFirstPlayer();
-			Player toReturn = this.toReturn;
-			this.toReturn = null;
-			return toReturn;
-		} catch (InvocationTargetException | InterruptedException e)
-		{
-			throw new RuntimeException(e);
-		}
+	{	
+		return GameView.getGameView(true).getFirstPlayer();
 	}
 	
 	@Override
